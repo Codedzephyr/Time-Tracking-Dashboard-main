51f251e5ac963e44bb565bfa013c1666d3da86ee
unknown <salihu45@gmail.com>
2022-01-04 19:23:09 +0100

    changes

--- src/design/Components/Homepage.jsx
+++ src/design/Components/Homepage.jsx
@@ -1,12 +1,70 @@
 import React from "react";
 import "./main.css";
+import avatar from "../../images/image-jeremy.png";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
-  return <main>
+  return (
+    <main>
       <div className="reportContainer">
-          
+        <div className="gridContainer">
+          <div className="mainProfileContainer">
+            <div className="profileContainer">
+              <img src={avatar} alt="jeremy" className="avatar" />
+              <p className="title1">Report for</p>
+              <p className="content">
+                Jeremy <br /> Robson
+              </p>
+            </div>
+            <div className="timeContainer">
+              <ul className="trackingList">
+                <li className="trackingListItem">
+                  <Link to="#">Daily</Link>
+                </li>
+                <li className="trackingListItem">
+                  <Link to="#">Weekly</Link>
+                </li>
+                <li className="trackingListItem">
+                  <Link to="#">Daily</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="workContainer"></div>
+          <div className="playContainer">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+            possimus, illo provident nisi omnis explicabo porro inventore
+            debitis minus perspiciatis? Inventore facilis molestiae deleniti,
+            repudiandae porro corrupti aperiam beatae perferendis?
+          </div>
+          <div className="studyContainer">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+            possimus, illo provident nisi omnis explicabo porro inventore
+            debitis minus perspiciatis? Inventore facilis molestiae deleniti,
+            repudiandae porro corrupti aperiam beatae perferendis?
+          </div>
+          <div className="exerciseContainer">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+            possimus, illo provident nisi omnis explicabo porro inventore
+            debitis minus perspiciatis? Inventore facilis molestiae deleniti,
+            repudiandae porro corrupti aperiam beatae perferendis?
+          </div>
+          <div className="socialContainer">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+            possimus, illo provident nisi omnis explicabo porro inventore
+            debitis minus perspiciatis? Inventore facilis molestiae deleniti,
+            repudiandae porro corrupti aperiam beatae perferendis?
+          </div>
+          <div className="selfCareContainer">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+            possimus, illo provident nisi omnis explicabo porro inventore
+            debitis minus perspiciatis? Inventore facilis molestiae deleniti,
+            repudiandae porro corrupti aperiam beatae perferendis?
+          </div>
+        </div>
       </div>
-  </main>;
+    </main>
+  );
 };
 
 export default Homepage;
